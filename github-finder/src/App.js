32542295764ea37538d01c00e668a1315c7e58b6
Nicolairulez <nicolairulez@gmail.com>
2020-07-23 21:52:37 -0400

    Alert and Not Found page, as well as Refactoring

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,54 +1,40 @@
-import React, { useState, Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
+import Home from './components/pages/Home'
 import User from './components/users/User'
+import NotFound from './components/pages/NotFound'
 import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
 import './App.css'
 
 const App = () => {
-  const [repos, setRepos] = useState([]),
-    [loading, setLoading] = useState(false),
-    [alert, setAlert] = useState(null)
-
-
-
-  // clear users
-
-  // set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type })
-    setTimeout(() => setAlert(null), 5000)
-  }
-
+  
   return (
     <GithubState>
+      <AlertState>
       <Router>
         <div className='App'>
           <Navbar />
           <div className='container'>
-            <Alert alert={alert} />
+            <Alert />
             <Switch>
               <Route
                 exact
                 path='/'
-                render={props => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
+                component={Home}
               />
               <Route exact path='/about' component={About} />
               <Route exact path={`/user/:login`} component={User}/>
+              <Route component={NotFound}/>
             </Switch>
           </div>
         </div>
       </Router>
+      </AlertState>
     </GithubState>
   )
 }
