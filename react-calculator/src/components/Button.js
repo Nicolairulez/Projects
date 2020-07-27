f34c2ddfbfcc99b1142bb408da503323f3e00ebb
Nicolairulez <nicolairulez@gmail.com>
2020-07-26 20:14:54 -0400

    finished design and clear button logic

--- react-calculator/src/components/Button.js
+++ react-calculator/src/components/Button.js
@@ -5,10 +5,8 @@ const isOperator = val => {
     return !isNaN(val) || val === '.' || val === '='
 }
 
-const Button = (props) => 
+export const Button = (props) => 
     
-<div className={`btn-wrapper ${isOperator(props.children) ? null : "operator"}`}>{props.children}</div>
+<div className={`btn-wrapper ${isOperator(props.children) ? null : "operator"}`} onClick={() => props.handleClick(props.children) }>{props.children}</div>
     
 
-
-export default Button
--- react-calculator/src/components/Input.js
+++ react-calculator/src/components/Input.js
@@ -2,8 +2,5 @@ import React from 'react'
 import './Input.css'
 
 
-const Input = () => {
+export const Input =props => <div className="input">{props.input}</div>
 
-}
-
-export default Input
